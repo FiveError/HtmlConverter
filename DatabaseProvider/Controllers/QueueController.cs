7216d19ff1e96a2701d6e4f2e456bafbb4a4341e
FiveError <ser9800@icloud.com>
2022-10-12 17:49:01 +0300

    Add API for queues: add to queue, remove from queue, get current document in queue

---
+++ DatabaseProvider/Controllers/QueueController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+
+namespace DatabaseProvider.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QueueController : ControllerBase
+    {
+        [HttpPost("{id_converter}")]
+        public string AddDocumentToQueue(int id_converter)
+        {
+            string filename = "";
+            DB.Instance.RunSqlRequestQuery("BEGIN TRANSACTION");
+            string SqlRequest = $"SELECT * FROM documents LIMIT 1";
+            var result = DB.Instance.RunSqlRequestReader(SqlRequest);
+            while (result.Read())
+            {
+                filename = Convert.ToString(result["filename"]);
+            }
+            if (!string.IsNullOrEmpty(filename))
+            {
+                DB.Instance.RunSqlRequestQuery($"INSERT INTO queues (id_converter,filename) VALUES({id_converter},'{filename}')");
+                DB.Instance.RunSqlRequestQuery($"DELETE FROM documents WHERE filename = '{filename}'");
+            }
+            DB.Instance.RunSqlRequestQuery("COMMIT");
+            return filename;
+        }
+
+        [HttpGet("{id_converter}")]
+        public string GetDocumentFromQueue(int id_converter)
+        {
+            string filename = "";
+            DB.Instance.RunSqlRequestQuery("BEGIN TRANSACTION");
+            string SqlRequest = $"SELECT * FROM queues WHERE id_convert = {id_converter}";
+            var result = DB.Instance.RunSqlRequestReader(SqlRequest);
+            while (result.Read())
+            {
+                filename = Convert.ToString(result["filename"]);
+            }
+            DB.Instance.RunSqlRequestQuery("COMMIT");
+            return filename;
+        }
+
+       [HttpDelete("{id_converter}")]
+       public string RemoveFromQueue(int id_converter)
+        {
+            string filename = "";
+            DB.Instance.RunSqlRequestQuery("BEGIN TRANSACTION");
+            string SqlRequest = $"SELECT * FROM queues WHERE id_converter = {id_converter}";
+            var result = DB.Instance.RunSqlRequestReader(SqlRequest);
+            while (result.Read())
+            {
+                filename = Convert.ToString(result["filename"]);
+            }
+            if (!string.IsNullOrEmpty(filename))
+            {
+                DB.Instance.RunSqlRequestQuery($"DELETE FROM queues WHERE id_converter = {id_converter}");
+            }
+            DB.Instance.RunSqlRequestQuery("COMMIT");
+            return filename;
+        }
+
+       [HttpPost("{filename}")]
+       public int InsertFilenameToDocuments(string filename)
+        {
+            string SqlRequest = $"INSERT INTO documents (filename) VALUES ('{filename}')";
+            DB.Instance.RunSqlRequestQuery(SqlRequest);
+            return 0;
+        }
+
+    }
+}
---
+++ DatabaseProvider/DB.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace DatabaseProvider
+{
+    public class DB
+    {
+        private static readonly Object s_lock = new Object();
+        private static DB instance = null;
+
+        private string connectionDB = "Data Source=/app/queue.db;Mode=ReadWriteCreate;";
+        private SqliteConnection connection;
+        private DB()
+        {
+            connection = new SqliteConnection(connectionDB);
+        }
+
+        public static DB Instance
+        {
+            get
+            {
+                if (instance != null)
+                    return instance;
+                Monitor.Enter(s_lock);
+                DB temp = new DB();
+                Interlocked.Exchange(ref instance, temp);
+                Monitor.Exit(s_lock);
+                return instance;
+            }
+        }
+
+        public void RunSqlRequestQuery(string Request)
+        {
+            OpenConnection();
+            SqliteCommand command = new SqliteCommand();
+            command.Connection = connection;
+            command.CommandText = Request;
+            command.ExecuteNonQuery();
+
+        }
+        public SqliteDataReader? RunSqlRequestReader(string Request)
+        {
+            OpenConnection();
+            SqliteCommand command = new SqliteCommand();
+            command.Connection = connection;
+            command.CommandText = Request;
+            return command.ExecuteReader();
+
+        }
+
+        private void OpenConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+                connection.Open();
+        }
+
+    }
+}
